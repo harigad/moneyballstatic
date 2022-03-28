8570533ff57e2164bf055382c946ae7065f7d95e
harigad <hgadiraju@gmail.com>
2022-03-28 09:07:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json({
-    "minimum_version": "0.4.3",
+    "minimum_version": "0.4.0",
     "upgrade_url": "https://app.bitrise.io/artifact/119172817/p/9ec1f159e32ba9ffedec9bf84e7ad045",
     "can_skip": true
   }
