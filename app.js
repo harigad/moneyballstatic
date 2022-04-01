9b3e96a919861fd26f6694135cffef800e3d92fe
harigad <hgadiraju@gmail.com>
2022-04-01 08:13:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.json({
     "minimum_version": "0.4.4",
     "upgrade_url": "https://app.bitrise.io/artifact/119791611/p/eedc20f1558010e10cad7ed842fd686a?source=ship",
-    "can_skip": true
+    "can_skip": false
   }
   );
 })
