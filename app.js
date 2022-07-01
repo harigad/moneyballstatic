22601525150ef3a33e72567c65ec5df2a8293983
tezzaworks <108304000+tezzaworks@users.noreply.github.com>
2022-07-01 01:30:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json({
-    "minimum_version": "0.5.15",
-    "upgrade_url": "https://app.bitrise.io/artifact/133462120/p/019ce99b057b86a037168fd0df71e67e?source=ship",
+    "minimum_version": "0.5.16",
+    "upgrade_url": "https://app.bitrise.io/artifact/134197567/p/a049f31a008c17ecf0aa2abab9831004?source=ship",
     "can_skip": false
   }
   );
